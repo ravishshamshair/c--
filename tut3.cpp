bd9b18a3fa0731ef04c6363100973c459b342f2e
ravishshamshair <ravishshamshair@gmail.com>
2026-02-02 22:41:25 -0800

    add new file

--- tut3.cpp
+++ tut3.cpp
@@ -2,8 +2,14 @@
 using namespace std;
 
 // This program was created by Ravish
+/* this
+is
+a
+multip
+line
+comment*/
 int main(){
-    cout<<"Hello world";
+    int sum = 6;
+    cout<<"Hello world"<< sum;
     return 0;
-
 }
