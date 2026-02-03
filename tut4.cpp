ca81b2f08cbc489858f13f195ab68a0621340c37
ravishshamshair <ravishshamshair@gmail.com>
2026-02-03 01:07:37 -0800

    data type & varaiable scope in c++

---
+++ tut4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+int glo = 6;
+void sum(){
+    int a;
+}
+int main(){
+int glo=9;
+glo=78;
+    //int a = 14
+    //int b = 15
+    int a=14, b =15;
+    float pi = 3.14;
+    char c = 'd';
+    bool is_true = true;
+    sum();
+    cout<<glo<< is_true;
+    //cout<<"this is tutorial 4.\n Here the value of a is "<<a<<". \nThe value of b is "<<b<<endl;
+    //cout<<"\nThe value of pi is "<<pi<<endl;
+    //cout<<"\nThe value of c is "<<c<<endl;   
+    return 0;  
+}
