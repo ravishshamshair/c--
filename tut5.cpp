49b8a20dca7cb767af48eae4acc8d75932946e96
ravishshamshair <ravishshamshair@gmail.com>
2026-02-04 01:12:29 -0800

    c++ basic input & output

---
+++ tut5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int num1, num2;
+    cout<<"enter the value of num 1:\n"; /* '<<' is called Insertion opertion */
+    cin>>num1; /* '>>' is called EXTRACTION OPERATOR */
+
+cout<<"enter the value of num 2:\n";/*'<<' is called insertion operation*/
+cin>>num2; /*'>>' is called  Extraction opertion*/
+
+cout<<"The sum is "<<num1+num2;    
+    
+}
