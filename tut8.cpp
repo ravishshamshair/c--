57f0b0a71928c2e1c85aca138ba8adb3c1163883
ravishshamshair <ravishshamshair@gmail.com>
2026-02-06 01:09:36 -0800

    constants mainpulator operation precedence in C++

---
+++ tut8.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+  //  int a = 34;
+    //cout<<"The value of a was: "<<a;
+   // a = 45;
+    //cout<<"the value of a is:"<<a;
+//constant in C++
+//const int a = 3;
+//cout<<"the value of a is:"<<a;
+// a = 45; // you will get an error because a is a constant
+//cout<<" the value of a is:"<<a<<endl;
+
+//mainpulator in C++
+//int a = 3, b = 4, c =1233;
+//cout<<"the value of a without setw is: "<<a<<endl;
+//cout<<"the valuw of b without setw is: "<<b<<endl;
+//cout<<" the value of c without setw is: "<<c<<endl;
+
+//cout<<"the value of a is: "<<setw(4)<<a<<endl;
+//cout<<" the value of b is: "<<setw(4)<<b<<endl;
+//cout<<"the value o c is: "<<setw(4)<<c<<endl;
+
+
+//operator precedence
+int a =3, b =4;
+//int c = (a*5)=b;
+int c = ((((a*5)+b)-45)+87);
+
+    return 0;
+
+}
