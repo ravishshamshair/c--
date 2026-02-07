c6247be4557f5cbdb7b193f199f3515edfce503b
ravishshamshair <ravishshamshair@gmail.com>
2026-02-07 01:43:52 -0800

    for while and do while loop C++

---
+++ tut10.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    /*loops in C++
+    There are three types of Loops in C++:
+1. for loop
+2. while loop
+3. do while - loop
+*/
+
+/*for in C++*/
+//int i=1;
+//cout<<i;
+//i++;
+
+// syntax of for for loop
+//for (initializer; condition ; updation)
+// {
+//   loop body (C++ code);
+//}
+
+
+
+//for (int i = 0; i < 40; i++) {
+  //  cout << i << endl;
+//}
+
+/* while loop in C++*/
+// synatax:
+// while (condition)
+//{
+//     C++ statement;
+//}
+  // printing 1 to 40 using while loop
+  int i = 1;
+  while (i <=40){
+    cout << i << endl;
+    i++;
+
+  }
+
+  /* do while Loop*/
+  // syntax:
+  //do
+  //{
+  //   C++ statements;
+  //} while (condition);
+// printing 1 to 40 using do while loop
+ //int i =1;
+ //do{
+   // cout<<i<<endl;
+   // i++;
+ //}while(false);
+
+ 
+   
+return 0;
+}
