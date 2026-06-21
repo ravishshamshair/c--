f0f6ef65a7fbd4088e0cc621941cfee9f39f8a1b
ravishshamshair <ravishshamshair@gmail.com>
2026-06-21 01:06:32 -0700

    objects Memory Allocation & using Arrays in classes in c++

--- tut1133.cpp
+++ tut1133.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
+
+    class shop{
+        int itrmId{100};
+        int itemPtice{100};
+        int counter;
+        public:
+        void initcounter(void) { counter =0;}
+        void getPrice(void);
+        void setPrice(void);
+    };
+
+    void Shop :: getPrice(void)
+    {
+        cout<<"Enter Id of your item"<<endl;
+        cin>>itemId[counter];
+    }
 int main(){
-
-    int x = 15;
-    int* p = &x;
-    *p = *p + 1;
-
-    cout<<x;
 
     return 0;
 
