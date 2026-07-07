1b0cf9bddbae629dce0740d1b8e5611d0cb19141
ravishshamshair <ravishshamshair@gmail.com>
2026-07-07 13:12:55 -0700

    Constructor Overloding In C++

---
+++ tut31.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+class complex
+{
+
+    int a, b;
+
+public:
+complex(){
+    a = 0;
+    b = 0;
+}
+    complex(int x, int y)
+    {
+
+        a = x;
+        b = y;
+    }
+    complex(int x){
+        a = x;
+        b = 0;
+    }
+    void printNumber()
+    {
+        cout << "your Number is " << a << " + " << b << "i" << endl;
+    }
+};
+int main()
+{
+    complex c1(4, 6);
+    c1.printNumber();
+
+    complex c2(5);
+    c2.printNumber();
+    return 0;
+}
